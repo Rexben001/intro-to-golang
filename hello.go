9f07c5c72eedb0c45bc9a462229ef79d72fd24e3
Rexben <rexben.rb@gmail.com>
2020-08-20 22:27:25 +0100

    add pointers

--- hello.go
+++ hello.go
@@ -1,13 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-)
+import "fmt"
 
 func main() {
 
@@ -37,27 +30,38 @@ func main() {
 	// fmt.Scanln(&myString)
 	// fmt.Println(myString)
 
-	//use bufio to read inputs
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your full name: ")
-	myname, _ := reader.ReadString('\n')
+	// //use bufio to read inputs
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter your full name: ")
+	// myname, _ := reader.ReadString('\n')
 
-	readertwo := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your full rating: ")
-	rating, _ := readertwo.ReadString('\n')
-	// convert string to number and remove extra space
-	ratingTwo, _ := strconv.ParseFloat(strings.TrimSpace(rating), 64)
+	// readertwo := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter your full rating: ")
+	// rating, _ := readertwo.ReadString('\n')
+	// // convert string to number and remove extra space
+	// ratingTwo, _ := strconv.ParseFloat(strings.TrimSpace(rating), 64)
 
-	// handle
-	fmt.Printf("Hello %v, \n we have received your rating %v. \n Thanks. \n\n Rating was recorded at %v", myname, ratingTwo, time.Now().Format(time.Stamp))
-	fmt.Println()
+	// // handle conditionals
+	// fmt.Printf("Hello %v, \n we have received your rating %v. \n Thanks. \n\n Rating was recorded at %v", myname, ratingTwo, time.Now().Format(time.Stamp))
+	// fmt.Println()
 
-	if ratingTwo == 4 {
-		fmt.Println("Wait now")
-	} else if ratingTwo > 4 {
-		fmt.Println("Opo juuu")
-	} else {
-		fmt.Println("Thanks for banking with us")
-	}
+	// if ratingTwo == 4 {
+	// 	fmt.Println("Wait now")
+	// } else if ratingTwo > 4 {
+	// 	fmt.Println("Opo juuu")
+	// } else {
+	// 	fmt.Println("Thanks for banking with us")
+	// }
+
+	// pointers
+	var total float64 = 33.2
+
+	// & reference to the memory location
+	totalRef := &total
+
+	fmt.Println(total, totalRef)
+
+	// * gets the value instead of the memory reference
+	fmt.Println(*totalRef)
 
 }
