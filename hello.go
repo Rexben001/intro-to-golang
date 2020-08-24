2562053317c04f815ea165248c0831c757c6e5ca
Rexben <rexben.rb@gmail.com>
2020-08-24 22:25:34 +0100

    writing files

--- hello.go
+++ hello.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strconv"
 	"time"
@@ -294,4 +293,23 @@ func main() {
 	// 	return
 	// }
 	// fmt.Println("Contents of file:", string(data))
+
+	// // Writing files
+	// f, err := os.Create("file1.txt")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// l, err := f.WriteString("Write Line one")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	f.Close()
+	// 	return
+	// }
+	// fmt.Println(l, "bytes written")
+	// err = f.Close()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 }
