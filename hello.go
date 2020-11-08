142bd8f4004f114d34826919b6f9b8c9887b36ba
Rexben <rexben.rb@gmail.com>
2020-11-08 23:54:29 +0100

    add gouroutine examples

--- hello.go
+++ hello.go
@@ -442,3 +442,89 @@ numArrs = append(numArrs, arr2)
 func changeVal (str *string){
 	*str = "changed"
 }
+
+// parallelism is different from concurrency. Parallelism means doing several things at the same time, concurrency is when you can do several things. Like jogging, tie loosed shoes, continue jogging.
+
+// Goroutines communicates using channels
+
+// data := <- a // read from channel a  
+// a <- data // write to channel a  
+
+/*
+Normal code
+package main
+
+import (  
+    "fmt"
+    "time"
+)
+
+func hello() {  
+    fmt.Println("Hello world goroutine")
+}
+func main() {  
+    go hello()
+    time.Sleep(1 * time.Second)
+    fmt.Println("main function")
+}
+*/
+
+/*
+Writing with Channels
+package main
+
+import (  
+    "fmt"
+)
+
+func hello(done chan bool) {  
+    fmt.Println("Hello world goroutine")
+    done <- true
+}
+func main() {  
+    done := make(chan bool)
+    go hello(done)
+    <-done
+    fmt.Println("main function")
+}
+*/
+
+/*
+
+Another channelgoroutine example
+package main
+
+import (  
+    "fmt"
+)
+
+func calcSquares(number int, squareop chan int) {  
+    sum := 0
+    for number != 0 {
+        digit := number % 10
+        sum += digit * digit
+        number /= 10
+    }
+    squareop <- sum
+}
+
+func calcCubes(number int, cubeop chan int) {  
+    sum := 0 
+    for number != 0 {
+        digit := number % 10
+        sum += digit * digit * digit
+        number /= 10
+    }
+    cubeop <- sum
+} 
+
+func main() {  
+    number := 589
+    sqrch := make(chan int)
+    cubech := make(chan int)
+    go calcSquares(number, sqrch)
+    go calcCubes(number, cubech)
+    squares, cubes := <-sqrch, <-cubech
+    fmt.Println("Final output", squares + cubes)
+}
+*?
